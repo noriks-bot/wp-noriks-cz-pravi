2cf759fea9f4814cb0736eade6a4297948daceb1
Noriks (via Claude) <info@noriks.com>
2026-07-27 07:45:15 +0200

    Thankyou upsell: crne -> sive bokserice (lokalizirano ime, isti IDs strukture/cene, slika siva produktna; 3x/5x kompozitni TODO); cene in logika nespremenjene

--- wp-content/themes/noriks/woocommerce/checkout/thankyou.php
+++ wp-content/themes/noriks/woocommerce/checkout/thankyou.php
@@ -51,14 +51,14 @@ if ( $upsell_is_majice ) {
     );
     $upsell_title_text = 'Přidejte trička teď – 50% sleva';
 } else {
-    $upsell_product_id = 4749; // Černé boxerky
-    $upsell_name       = 'Černé boxerky';
+    $upsell_product_id = 4766; // Šedé boxerky (siva verzija, ista struktura kot crna)
+    $upsell_name       = 'Šedé boxerky';
     $upsell_qty_prices = array( 1 => 119, 3 => 357, 5 => 595 );
-    $upsell_qty_names  = array( 1 => '1x Černé boxerky', 3 => '3x Černé boxerky', 5 => '5x Černých boxerek' );
+    $upsell_qty_names  = array( 1 => '1x Šedé boxerky', 3 => '3x Šedé boxerky', 5 => '5x Šedých boxerek' );
     $upsell_qty_images = array(
-        1 => 'https://noriks.com/hr/wp-content/uploads/2025/11/crne-boksarice-produktna.jpg',
-        3 => 'https://noriks.com/hr/wp-content/uploads/2025/11/boksarice_3x_crne.png',
-        5 => 'https://noriks.com/hr/wp-content/uploads/2026/01/boksarice_5x_crne.png',
+        1 => 'https://noriks.com/cz/wp-content/uploads/2026/01/sive-boksarice-produktna.jpg',
+        3 => 'https://noriks.com/cz/wp-content/uploads/2026/01/sive-boksarice-produktna.jpg', // TODO: 3x siva kompozitna, ko obstaja
+        5 => 'https://noriks.com/cz/wp-content/uploads/2026/01/sive-boksarice-produktna.jpg', // TODO: 5x siva kompozitna, ko obstaja
     );
     $upsell_title_text = 'Přidejte boxerky teď – 50% sleva';
 }
